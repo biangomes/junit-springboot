6b5d7d8b268929a2e606accd6d3b5ef302048f27
Beatriz Nascimento Gomes <beatriz.ngomes@luizalabs.com>
2024-10-08 21:28:33 -0300

    Adiciona alguns cases de teste

---
+++ src/test/java/com/example/junitspringboot/math/SimpleMathTest.java
@@ -0,0 +1,76 @@
+package com.example.junitspringboot.math;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleMathTest {
+
+  SimpleMath math = new SimpleMath();
+
+  @Test
+  void sum_Success() {
+    Double n1 = 5.0;
+    Double n2 = 10.0;
+    Double sum = math.sum(n1, n2);
+    Assertions.assertEquals(15.0, sum);
+  }
+
+  @Test
+  void sum_OrderDoesntMatter() {
+    Double n1 = 5.0;
+    Double n2 = 10.0;
+    Double sum1 = math.sum(n1, n2);
+    Double sum2 = math.sum(n2, n1);
+    Assertions.assertEquals(sum1, sum2);
+  }
+
+  @Test
+  void sum_NegativeNumber() {
+    Double n1 = -3.0;
+    Double n2 = 2.0;
+    Double sum = math.sum(n1, n2);
+    Assertions.assertEquals(-1, sum);
+  }
+
+  @Test
+  void subtraction_Success() {
+    Double n1 = 10.0;
+    Double n2 = 5.0;
+    Double sub = math.subtraction(n1, n2);
+    Assertions.assertEquals(5.0, sub);
+  }
+
+  @Test
+  void subtraction_NegativeResult() {
+    Double n1 = 5.0;
+    Double n2 = 10.0;
+    Double sub = math.subtraction(n1, n2);
+    Assertions.assertEquals(-5.0, sub);
+  }
+
+  @Test
+  void subtraction_OrderMatters() {
+    Double n1 = 10.0;
+    Double n2 = 5.0;
+    Double sub1 = math.subtraction(n1, n2);
+    Double sub2 = math.subtraction(n2, n1);
+    Assertions.assertNotEquals(sub1, sub2);
+  }
+
+  @Test
+  void multiplication_Success() {
+    Double n1 = 10.0;
+    Double n2 = 5.0;
+    Double mult = math.multiplication(n1, n2);
+    Assertions.assertEquals(50.0, mult);
+  }
+
+  @Test
+  void multiplication_OrderDoesntMatter() {
+    Double n1 = 10.0;
+    Double n2 = 5.0;
+    Double mult1 = math.multiplication(n1, n2);
+    Double mult2 = math.multiplication(n2, n1);
+    Assertions.assertEquals(mult1, mult2);
+  }
+}
